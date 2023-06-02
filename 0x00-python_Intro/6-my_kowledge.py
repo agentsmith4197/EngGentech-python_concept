42403d765aac3af2929380b9f9c0c65cb9683091
agentsmith4197 <87205529+agentsmith4197@users.noreply.github.com>
2023-06-02 08:48:31 +0100

    Update 6-my_kowledge
    add

--- 0x00-python_Intro/6-my_kowledge.py
+++ 0x00-python_Intro/6-my_kowledge.py
@@ -1,5 +1,12 @@
 smith = "argument passing in pyrhon refers to the process of passing values or reference to a function or method when it is called. Arguments are used to provide input data to the function, which can then perform operations on that data. there are two types arguments: postional arguments and keyword arguments."
-example = "keyword Arguments:" \
-          "def smith(name, age):" \
-          "print(""Hello,"+ name + "! You)"
+smith_example =  "Positional Arguments:
+greet("Alice", 25)
+def smart(name, age):
+    print("Hello, " + name + "! You are " + str(age) + " years old.")
+Keyword Arguments:
+smart(age=30, name="Bob")
+def smart(name, age):
+    print("Hello, " + name + "! You are " + str(age) + " years old.")"
+
 print(smith)
+print(smith_example)
