5f1038a905be6986f8f362f58c277a82b9c9aae5
agentsmith4197 <87205529+agentsmith4197@users.noreply.github.com>
2023-06-07 17:57:33 +0100

    Update 6-my_kowledge

--- 0x00-python_Intro/6-my_kowledge.py
+++ 0x00-python_Intro/6-my_kowledge.py
@@ -1,12 +1,13 @@
-smith = "argument passing in pyrhon refers to the process of passing values or reference to a function or method when it is called. Arguments are used to provide input data to the function, which can then perform operations on that data. there are two types arguments: postional arguments and keyword arguments."
-smith_example =  "Positional Arguments:
-greet("Alice", 25)
+smith = """argument passing in pyrhon refers to the process of passing values or reference to a function or method when it is called. Arguments are used to provide input data to the function, which can then perform operations on that data. there are two types arguments: postional arguments and keyword arguments."""
+smith_example =  """Positional Arguments:
+
 def smart(name, age):
     print("Hello, " + name + "! You are " + str(age) + " years old.")
+smart("Alice", 25)
 Keyword Arguments:
-smart(age=30, name="Bob")
-def smart(name, age):
-    print("Hello, " + name + "! You are " + str(age) + " years old.")"
 
+def smart(name, age):
+    print("Hello, " + name + "! You are " + str(age) + " years old.")
+smart(age=30, name="Bob")"""
 print(smith)
 print(smith_example)
